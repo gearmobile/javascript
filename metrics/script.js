ae7198ebb50285d671a34aca5f6e8c9d9669cb85
Valery Semenencko <gearmobile@gmail.com>
2015-12-23 16:20:41 +0300

    metrics

--- metrics/script.js
+++ metrics/script.js
@@ -1,17 +1,15 @@
 
 // ФУНКЦИЯ ОПРЕДЕЛЕНИЯ РАЗМЕРА ПРОКРУТКИ ЭЛЕМЕНТА СНИЗУ
+
 function scrollBottom(el) {
 	return el.scrollHeight - ( el.scrollTop + el.clientHeight );
 }
 
-
-
-
-
+// Здесь используется метрика `clientHeight`, а не `offsetHeight`,
+// так как нужно не учитывать `border`.
 
 var block = document.querySelector('.block');
 
 bottomScroll = scrollBottom(block);
-console.log(bottomScroll);
 
 
