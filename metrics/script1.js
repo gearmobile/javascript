f28f48b4a35f01439621b28b07bf734c07473527
Valery Semenencko <gearmobile@gmail.com>
2016-01-30 13:00:00 +0300

    metrics

---
+++ metrics/script1.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function () {
+
+	var moved = document.getElementById('moving-div');
+	moved.style.position = 'absolute';
+	moved.style.right = moved.style.top = 0;
+
+	var clone = document.createElement('div');
+	clone.style.width = moved.
+
+
+});
