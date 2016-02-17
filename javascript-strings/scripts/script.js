4ed220312e34f986ba5fb86b3ba231160b0b0c10
Valery Semenencko <gearmobile@gmail.com>
2016-02-17 16:17:34 +0300

    sorax

---
+++ javascript-strings/scripts/script.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+
+  var form  = document.getElementsByName('search-person')[0];
+  var table = document.getElementById('age-table');
+  var labels = form.getElementsByTagName('label');
+
+
+
+  console.log(labels.length);
+
+
+}
