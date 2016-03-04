50ab76834b630c1427ad477d3fa6a08dff5f2289
Valery Semenencko <gearmobile@gmail.com>
2016-03-04 13:39:03 +0300

    toggle-gallery - add third example

--- toogle-gallery/script.js
+++ toogle-gallery/script.js
@@ -8,21 +8,17 @@ $(function () {
   var $buttonMore = 'show more';
   var $buttonLess = 'show less';
 
-  jQuery.fn.toggleText = function ( $more, $less, $class ) {
-    $(this).on('click', function () {
-      $(this).text( ( $(this).text() === $more ) ? $less : $more ).toggleClass($class);
-    })
+  jQuery.fn.toggleText = function ( $more, $less ) {
+    $(this).text( ( $(this).text() === $more ) ? $less : $more );
   };
-
-  $button.text($buttonMore);
-  $button.toggleText( $buttonMore, $buttonLess, $buttonClass );
-
 
   // FIRST EXAMPLE
   // -------------------------------------------
 
   var flag = 'short';
   var $col = $('.collection > li');
+
+  $button.text($buttonMore);
 
   $col.each( function ( index ) {
     if ( index < $col.length / 2 ) {
@@ -33,6 +29,7 @@ $(function () {
   });
 
   $button.on('click', function () {
+    $(this).toggleClass($buttonClass).toggleText( $buttonMore, $buttonLess );
     flag = ( flag === 'short' ) ? 'full' : 'short';
     if ( flag === 'short' ) {
       $col.each( function ( index ) {
@@ -58,6 +55,22 @@ $(function () {
 
   $listButton.on('click', function () {
     $list.toggleClass('list-short');
+  });
+
+
+  // THIRD EXAMPLE
+  // ------------------------------------------
+
+  $('.main .readmore').on('click', function (event) {
+    var info = $('.content--more').is(':visible') ? 'show me more (+)' : 'show me less (-)';
+    $(this).html(info).parent().prev('.header').children('.content').children('.content--more').toggleClass('is-visible');
+    event.preventDefault();
+  });
+
+  $('.show .gallery__button').on('click', function (event) {
+    var message = $('.gallery__more').is(':visible') ? 'show me more ( + )' : 'show me less ( - )';
+    $(this).html(message).prev('.gallery').children('.gallery__more').toggleClass('is-visible');
+    event.preventDefault();
   });
 
 
