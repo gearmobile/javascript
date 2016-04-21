a078df06963f15cc54d81d4f6882bb35f3f93a20
gearmobile <gearmobile@gmail.com>
2016-04-21 09:07:57 +0300

    move canvas examples to canvas repo

--- canvas/imagedata/imagedata.js
+++
@@ -1,8 +0,0 @@
-var canvas = document.querySelector('#imagedata');
-
-if ( canvas.getContext ) {
-
-  canvas.width = 400;
-  canvas.height = 400;
-
-}
--- canvas/pattern/readme.md
+++
@@ -1,8 +0,0 @@
-# Canvas Pattern
-
-Допустимые методы повтора изображений:
-
-* `repeat`
-* `repeat-x`
-* `repeat-y`
-* `no-repeat`
