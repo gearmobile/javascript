7add1967edc4380219563747fec66be7029ecb0c
gearmobile <gearmobile@gmail.com>
2016-04-26 16:07:24 +0300

    specialist-lesson-5

---
+++ specialist-lesson-5/script.js
@@ -0,0 +1,21 @@
+/**
+ * Created by zencoder on 4/26/16.
+ */
+
+// parentNode - ссылка на родительский элемент
+// previousSibling - ссылка на предыдущий соседний элемент
+// nextSibling - ссылка на следующий соседний элемент
+// childNodes - коллекция дочерних элементов
+// firstChild - ссылка на первый дочерний элемент
+// lastChild - ссылка на последний дочерний элемент
+
+// nodeName - возвращает имя тега элемента
+// nodeType - возвращает тип элемента
+// nodeValue - свойство имеется только у текстовго узла и возвращает сам текст этого узла
+
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    var a = document.links[0];
+    console.log(a);
+});
--- webpack/frontend/welcome.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by zencoder on 4/10/16.
- */
-'use strict';
-
-module.exports = function (message) {
-  debugger;
-  alert('Welcome ' + message);
-};
