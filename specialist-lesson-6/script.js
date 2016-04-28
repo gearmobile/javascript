b47c4e8e3ca1a1ca97dfac0f857d9830c721f15f
gearmobile <gearmobile@gmail.com>
2016-04-28 10:03:27 +0300

    nodes

--- specialist-lesson-6/script.js
+++ specialist-lesson-6/script.js
@@ -2,18 +2,84 @@
  * Created by zencoder on 4/27/16.
  */
 
+// document.createElement
+// document.createTextNode
+// appendChild
+// insertBefore(x,y)
+// cloneNode(false|true)
+// replaceChild(x,y)
+
 window.addEventListener('DOMContentLoaded', function () {
+
   var paragraph = document.querySelector('#wrapper');
+
   var lastParagraph = document.createElement('p');
   var firstParagraph = document.createElement('p');
   var someParagraph = document.createElement('p');
+
   var someParagraphText = document.createTextNode('Some Paragraph');
   var lastParagraphText = document.createTextNode('Last paragraph');
   var firstParagraphText = document.createTextNode('First paragraph');
+
   lastParagraph.appendChild(lastParagraphText);
   firstParagraph.appendChild(firstParagraphText);
   someParagraph.appendChild(someParagraphText);
+
+  var clonedParagraph = someParagraph.cloneNode(true);
+
   paragraph.insertBefore(firstParagraph, paragraph.firstChild);
-  paragraph.insertBefore(someParagraph, paragraph.childNodes[2]);
+  paragraph.insertBefore(someParagraph, document.querySelector('#duo'));
   paragraph.appendChild(lastParagraph);
+  paragraph.insertBefore(clonedParagraph, document.querySelector('#tre'));
+
+  document.querySelector('#controls').appendChild(document.querySelector('#firstBtn').cloneNode(true));
+
+
+  var list = document.querySelector('#list');
+  var listItems = list.getElementsByTagName('li');
+  var removedListItem = list.removeChild(listItems[4]);
+  list.insertBefore(removedListItem, listItems[0]);
+
+  var replacedListItem = list.replaceChild(listItems[2], listItems[3]);
+
 }, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
