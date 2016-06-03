dc6fd4f50bddf4eb130bf95a069e8695092cce5f
gearmobile <gearmobile@gmail.com>
2016-06-03 16:20:57 +0300

    add dice

---
+++ dice/script.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    var buttonRoolDice = document.querySelector('.dice-roll');
+
+    function rollDice () {
+
+        var diceSide1 = document.getElementById('dice-side-1');
+        var diceSide2 = document.getElementById('dice-side-2');
+
+        var status = document.getElementById('status');
+
+        var side1 = Math.floor( Math.random() * 6 ) + 1;
+        var side2 = Math.floor( Math.random() * 6 ) + 1;
+
+        var diceTotal = side1 + side2;
+
+        diceSide1.innerHTML = side1;
+        diceSide2.innerHTML = side2;
+
+        status.innerHTML = 'You rolled ' + diceTotal + '.';
+
+        if ( side1 === side2 ) {
+            status.innerHTML += ' Doubles! You get a free turn!';
+        }
+    }
+
+    buttonRoolDice.addEventListener('click', rollDice, false);
+
+}, false);
