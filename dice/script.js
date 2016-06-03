d9cfe0181c5402dab8568e6a297786648a8c82aa
gearmobile <gearmobile@gmail.com>
2016-06-03 22:51:32 +0300

    dice game

--- dice/script.js
+++ dice/script.js
@@ -1,17 +1,13 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-    var buttonRoolDice = document.querySelector('.dice-roll');
-
     function rollDice () {
 
         var diceSide1 = document.getElementById('dice-side-1');
         var diceSide2 = document.getElementById('dice-side-2');
-
         var status = document.getElementById('status');
 
         var side1 = Math.floor( Math.random() * 6 ) + 1;
         var side2 = Math.floor( Math.random() * 6 ) + 1;
-
         var diceTotal = side1 + side2;
 
         diceSide1.innerHTML = side1;
@@ -24,6 +20,7 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    var buttonRoolDice = document.querySelector('.dice-roll');
     buttonRoolDice.addEventListener('click', rollDice, false);
 
 }, false);
