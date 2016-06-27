d572422a3142f9320bf88446d46e8755b9c6f598
gearmobile <gearmobile@gmail.com>
2016-06-27 17:48:04 +0300

    error

--- click/script.js
+++ click/script.js
@@ -3,14 +3,20 @@ window.addEventListener( 'DOMContentLoaded', function () {
     var block = document.querySelector('#block');
     var circle = document.querySelector('#circle');
 
-    block.addEventListener('click', function () {
-        circle.classList.add('is-active');
-    });
+    // CHANGE CIRCLE BACKGROUND-COLOR
+    // ---------------------------------------------------
+    block.addEventListener( 'click', function ( event ) {
+        if ( event.target.id !== circle.id ) {
+            circle.classList.add('is-active');
+        }
+    }, false );
 
+    // RETURN CIRCLE ORIGIN BACKGROUND-COLOR
+    // ----------------------------------------------------
     document.addEventListener( 'click', function ( event ) {
         if ( event.target.id !== block.id && event.target.id !== circle.id && circle.classList.contains('is-active') ) {
             circle.classList.remove('is-active');
         }
-    });
+    }, false );
 
-}, false);
+}, false );
