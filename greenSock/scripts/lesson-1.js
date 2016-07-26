6d609a786536cebf2ba74bac74d6bee62da38023
gearmobile <gearmobile@gmail.com>
2016-07-26 11:22:11 +0300

    greensock

--- greenSock/scripts/lesson-1.js
+++ greenSock/scripts/lesson-1.js
@@ -1,19 +1,27 @@
 window.addEventListener( 'DOMContentLoaded', function () {
 
     var header = document.querySelector( '#header' );
+    var listItemFirst = document.querySelector( '.list' ).children[0];
+    var listItemSecond = document.querySelector( '.list' ).children[1];
+    var image = document.querySelector( '#image' );
     var subHeader = document.querySelector( '#subheader' );
-    var listItemFirst = document.querySelector('.list').firstElementChild;
-    var listItemSecond = document.querySelector('.list').children[1];
     //var listItemLast = document.querySelector('.list').lastElementChild;
-    var image = document.querySelector('#image');
 
     // BASIC OVERVIEW
     // ------------------------------------------------------------
     TweenLite.to( header, 3, { opacity: 0, y: 100 } );
-    TweenLite.to( subHeader, 3, { y: -50, borderTopLeftRadius: 0, borderBottomRightRadius: 0, borderTopRightRadius: 40, borderBottomLeftRadius: 40, color: 'white', delay: 2 } );
     TweenLite.to( listItemFirst, 2, { x: 100, backgroundColor: 'red' } );
     TweenLite.to( listItemSecond, 2, { x: -100, backgroundColor: 'green' } );
     TweenLite.to( image, 2, { width: 600, height: 200, opacity: .5 } );
+    TweenLite.to( subHeader, 3, {
+        y: -50,
+        borderTopLeftRadius: 0,
+        borderBottomRightRadius: 0,
+        borderTopRightRadius: 40,
+        borderBottomLeftRadius: 40,
+        color: 'white',
+        delay: 2
+    });
 
     // METHOD TO(), METHOD FROMTO(), METHOD SET()
     // -------------------------------------------------------------
