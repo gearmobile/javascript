2b3c332aabbd80de39d398daefe6bc463fef44e3
gearmobile <gearmobile@gmail.com>
2016-08-24 19:37:57 +0300

    battleship

--- book-freeman-javascript/script.js
+++ book-freeman-javascript/script.js
@@ -1,12 +1,39 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener( 'DOMContentLoaded', function () {
 
-    var name = 'Joe';
-    var i = 0;
-    while ( i < 2 ) {
-        document.write('Happy birthday to you!<br>');
-        ++i;
+    function getRandomInt( min, max ) {
+        return Math.floor( Math.random() * (max - min + 1) ) + min;
     }
-    document.write('Happy birthday dear ' + name + '<br>');
-    document.write('Happy birthday to you!');
 
-}, false);
+    var grid = Array(7);
+    var shipLength = 3;
+    var cell;
+    var count;
+
+    function init() {
+        var rnd = getRandomInt( 0, grid.length - shipLength );
+        for ( var i = 0; i < shipLength; i++ ) {
+            grid[rnd] = 1;
+            rnd++;
+        }
+    }
+
+    function getValue() {
+        cell = prompt( 'Please enter number between 0 and 6', '1' );
+        cell = parseInt( cell );
+        if ( cell !== null && cell <= 6 ) {
+            return cell;
+        } else {
+            alert( 'Please enter correct number' );
+        }
+    }
+
+    function check(cell) {
+        //
+    }
+
+    init();
+    // getValue();
+
+    console.log(grid);
+
+}, false );
