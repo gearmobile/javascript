2995a5a399cbe338bc92e63ccee581e2b3afb7e4
gearmobile <gearmobile@gmail.com>
2016-09-11 12:25:06 +0300

    js

--- freecamp/case-84/script.js
+++ freecamp/case-84/script.js
@@ -1,5 +1,14 @@
 window.addEventListener( 'load', function () {
-    // -------------------------------------------------------
-    // -
-    // -------------------------------------------------------
+    //
+    function Person( name ){
+        this.name = name;
+    }
+    //
+    Person.prototype.greet = function ( otherName ) {
+        return 'Hi ' + otherName + ', my name is ' + this.name;
+    };
+    //
+    let mio = new Person('John');
+    //
+    console.log( mio.greet('Valery') );
 }, false );
