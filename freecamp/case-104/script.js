120e4e803a852f89166499400c4cb62a930c5118
Valery Semenencko <gearmobile@gmail.com>
2016-09-19 19:55:31 +0300

    js

--- freecamp/case-104/script.js
+++ freecamp/case-104/script.js
@@ -2,35 +2,38 @@ window.addEventListener( 'load', function () {
     // -------------------------------------------------------
     var contacts = [
         {
-            'firstName': 'Akira',
-            'lastName': 'Laine',
-            'number': '0543236543',
-            'likes': [ 'Pizza', 'Coding', 'Brownie Points' ]
+            firstName: 'Akira',
+            lastName: 'Laine',
+            number: '0543236543',
+            likes: [ 'Pizza', 'Coding', 'Brownie Points' ]
         },
         {
-            'firstName': 'Harry',
-            'lastName': 'Potter',
-            'number': '0994372684',
-            'likes': [ 'Hogwarts', 'Magic', 'Hagrid' ]
+            firstName: 'Harry',
+            lastName: 'Potter',
+            number: '0994372684',
+            likes: [ 'Hogwarts', 'Magic', 'Hagrid' ]
         },
         {
-            'firstName': 'Sherlock',
-            'lastName': 'Holmes',
-            'number': '0487345643',
-            'likes': [ 'Intriguing Cases', 'Violin' ]
+            firstName: 'Sherlock',
+            lastName: 'Holmes',
+            number: '0487345643',
+            likes: [ 'Intriguing Cases', 'Violin' ]
         },
         {
-            'firstName': 'Kristian',
-            'lastName': 'Vos',
-            'number': 'unknown',
-            'likes': [ 'Javascript', 'Gaming', 'Foxes' ]
+            firstName: 'Kristian',
+            lastName: 'Vos',
+            number: 'unknown',
+            likes: [ 'Javascript', 'Gaming', 'Foxes' ]
         }
     ];
 
 
-    function lookUpProfile( firstName, prop ){
-        //
+    function lookUpProfile( object ){
+        for ( let i = 0; i < object.length; i++ ) {
+            return object[i];
+        }
     }
+    console.log( lookUpProfile( contacts ) );
      // Change these values to test your function
     lookUpProfile( 'Akira', 'likes' );
     // -------------------------------------------------------
--- freecamp/case-97/script.js
+++ freecamp/case-97/script.js
@@ -1,48 +1,62 @@
 window.addEventListener( 'load', function () {
     // -------------------------------------------------------
     var collection = {
-        "2548": {
-            "album": "Slippery When Wet",
-            "artist": "Bon Jovi",
-            "tracks": [ "Let It Rock", "You Give Love a Bad Name" ]
+        2548: {
+            album: 'Slippery When Wet',
+            artist: 'Bon Jovi',
+            tracks: [ 'Let It Rock', 'You Give Love a Bad Name' ]
         },
-        "2468": {
-            "album": "1999",
-            "artist": "Prince",
-            "tracks": [ "1999", "Little Red Corvette" ]
+        2468: {
+            album: '1999',
+            artist: 'Prince',
+            tracks: [ '1999', 'Little Red Corvette' ]
         },
-        "1245": {
-            "artist": "Robert Palmer",
-            "tracks": []
+        1245: {
+            artist: 'Robert Palmer',
+            tracks: []
         },
-        "5439": {
-            "album": "ABBA Gold"
+        5439: {
+            album: 'ABBA Gold'
         }
     };
-
-    function updateRecords ( id, prop, value ) {
-
-        let key = collection[id];
-
-        if ( value === false ) {
-            delete collection[id][prop];
-        }
-
-        if ( key[prop] === undefined ) {
-            key[prop] = [];
-        }
-
-        if ( key[prop].length === 0 && value !== false ) {
-            key[prop].push( value );
-        }
-
-
-        return collection;
-
+    //
+    function getData() {
+        let xhr = new XMLHttpRequest();
+        xhr.open( 'GET', 'collection.json', true );
+        xhr.onreadystatechange = function () {
+            "use strict";
+            if ( xhr.readyState === 4 && xhr.status === 200 ) {
+                return xhr.responseText;
+            }
+        };
+        xhr.send();
     }
-    // -------------------------------------------------------
-    let colToString = JSON.stringify( collection );
-    let colToObject = JSON.parse( colToString );
-    // -------------------------------------------------------
-    console.log( updateRecords( 5439, 'artist', 'ABBA' ) );
+    document.getElementById('get').addEventListener( 'click', function () {
+        getData();
+    }, false );
+    //
+    let collectionCopy = JSON.parse( JSON.stringify( collection ) );
+    //
+    function updateRecords ( key, prop, value ) {
+        // if value is empty
+        if ( !value ) {
+            delete collection[key][prop];
+        }
+        //
+        if ( prop === 'tracks' && !collection[key][prop] && value ) {
+            collection[key][prop] = [];
+        }
+        // if key is tracks and value is not empty
+        if ( prop === 'tracks' && value ) {
+            collection[key][prop].push( value );
+        }
+        //
+        if ( prop !== 'tracks' && value ) {
+            collection[key][prop] = value;
+        }
+        return collection;
+    }
+    console.log( updateRecords( '5439', 'album', 'The Doors' ) );
+    console.log( updateRecords( '5439', 'tracks', 'Addicted to Love' ) );
+    console.log( updateRecords( '2548', 'tracks', '' ) );
 }, false );
