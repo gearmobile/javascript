85b6943ffc300758d6abab1320dd9de68b3cb773
gearmobile <gearmobile@gmail.com>
2016-09-21 15:55:03 +0300

    js forever

--- ajax/case-06/script.js
+++ ajax/case-06/script.js
@@ -3,10 +3,14 @@ window.addEventListener( 'load', function () {
     let xhr = new XMLHttpRequest();
     xhr.open( 'GET', 'rooms.json', true );
     xhr.responseText = 'text';
-    xhr.onload = function ( event ) {
-        if ( xhr.status === 200 ) {
+    xhr.onload = function () {
+        if ( xhr.readyState === 4 && xhr.status === 200 ) {
             rooms = JSON.parse( xhr.responseText );
             console.log( 'JSON data loaded' );
+            if ( rooms['livingRoom']['light']['on'] === true ) {
+                console.log( 'Living room light is on' );
+            }
         }
-    }
+    };
+    xhr.send();
 }, false );
