176e5bcc9d787248486f99ed17430176186b6222
gearmobile <gearmobile@gmail.com>
2016-09-23 16:21:43 +0300

    d3js - axis

--- d3js/case-32/script.js
+++ d3js/case-32/script.js
@@ -1,11 +1,34 @@
 window.addEventListener( 'load', function () {
+    //
     const width = 500;
-    const data = [ 10, 30, 12, 43, 78, 45, 83, 32, 87, 99 ];
+    const height = 500;
+    const data = [ 55, 44, 30, 23, 17, 14, 16, 25, 41, 61, 85, 101, 95, 105, 114, 150, 180, 210, 125, 100, 71, 75, 72, 67 ];
     let maxValue = d3.max( data );
+    //
+    let svg = d3.select('body').append('svg')
+        .classed( 'axis', true )
+        .attr({
+            width: width,
+            height: height
+        });
+    //
     let value = d3.scale.linear()
         .domain([ 0, maxValue ])
         .range([ 0, width ]);
     //
+    let axisGroup = svg.append( 'g' ).classed( 'axis-group', true );
     let axis = d3.svg.axis().scale( value );
-    svg.call( axis );
+    let axisNodes = axisGroup.call( axis );
+    let domain = axisNodes.selectAll('.domain');
+    domain.attr({
+        'fill': 'none',
+        'stroke-width': 1,
+        'stroke': 'black'
+    });
+    let ticks = axisNodes.selectAll('.tick line');
+    ticks.attr({
+        'fill': 'none',
+        'stroke-width': 1,
+        'stroke': 'black'
+    });
 }, false );
