83eb0f81c3a6792723eb0671c6aed6932c6c1f70
Valery Semenencko <gearmobile@gmail.com>
2016-09-25 18:43:58 +0300

    gsap

--- GSAP/case-01/app.js
+++ GSAP/case-01/app.js
@@ -6,20 +6,33 @@ window.addEventListener( 'load', function () {
     //
     function imageFuncReverse( object ) {
         if ( object ) {
-            TweenLite.to( object, 2, { delay: delay * 1.4, x: 0, y: 0, scale: 1 } );
+            TweenLite.to( object, 2, { delay: ( delay * 1.4 ), x: 0, y: 0, scale: 1 } );
         }
     }
     //
     function imageOneFunc() {
-        TweenLite.to( imageOne, 1, { delay: delay, x: 1000, y: -100, scale: 1.4, onComplete: imageFuncReverse, onCompleteParams: [ imageOne ] } );
+        TweenLite.to( imageOne, 1, {
+            delay: ( delay - 0.8 ),
+            x: 1000,
+            y: -100,
+            scale: 1.4,
+            onComplete: imageFuncReverse,
+            onCompleteParams: [ imageOne ]
+        });
     }
     //
     function imageTwoFunc() {
-        TweenLite.to( imageTwo, 2, { delay: delay, x: 100, y: 200, rotation: 180, alpha: 0.6 } );
+        TweenLite.to( imageTwo, 2, { delay: ( delay - 0.2 ), x: 1000, y: 200, rotation: 180, alpha: 0.6 } );
     }
     //
-    imageOneFunc();
-     //imageTwoFunc();
+    imageOne.addEventListener( 'mouseenter', function () {
+        imageOneFunc();
+    }, false );
+    //
+    imageTwo.addEventListener( 'mouseover', function () {
+        imageTwoFunc();
+    }, false );
+    //
 }, false );
 
 
