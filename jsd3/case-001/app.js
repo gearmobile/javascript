05f499265d7843cf4b73e6d54be5effc4909fc7c
gearmobile <gearmobile@gmail.com>
2016-09-25 21:34:33 +0300

    d3.js

---
+++ jsd3/case-001/app.js
@@ -0,0 +1,7 @@
+window.addEventListener( 'load', function () {
+    //
+    let diaWidth = 800;
+    let diaHeight = 400;
+    let data = [ 20, 50, 40, 12, 98, 56, 43, 67, 33, 7, 43, 59, 99, 10, 32, 45 ];
+    //
+}, false );
