42583170dc5aef9f4ec4c913f2ab8e5388a3bebf
gearmobile <gearmobile@gmail.com>
2016-09-30 15:30:25 +0300

    d3js - bar chart

--- jsd3/case-001/app.js
+++ jsd3/case-001/app.js
@@ -4,13 +4,17 @@ window.addEventListener( 'load', function () {
     let chartWidth = 800;
     let chartHeight = 400;
     let barPadding = 1;
-    //
+    // ----------------------------------------------------
+    // CHART
+    // ----------------------------------------------------
     let chart = d3.select('body')
         .append('svg')
         .attr( 'class', 'chart' )
         .attr( 'width', chartWidth )
         .attr( 'height', chartHeight );
-    //
+    // ----------------------------------------------------
+    // CHART BARS
+    // ----------------------------------------------------
     let chartBars = chart.selectAll('rect')
         .data( dataset )
         .enter()
@@ -28,5 +32,25 @@ window.addEventListener( 'load', function () {
         })
         .attr( 'width', function () {
             return chartWidth / dataset.length - barPadding;
+        });
+    // ----------------------------------------------------
+    // CHART BARS LABELS
+    // ----------------------------------------------------
+    let charLabels = chart.selectAll('text')
+        .data( dataset )
+        .enter()
+        .append('text')
+        .text( function ( d ) {
+                return d;
         })
+        .attr( 'x', function ( d, i ) {
+            return i * ( chartWidth / dataset.length ) + ( chartWidth / dataset.length - barPadding ) / 2 ;
+        })
+        .attr( 'y', function ( d ) {
+            return ( chartHeight - d * 3 ) + 14;
+        })
+        .attr( 'fill', 'white' )
+        .attr( 'font-family', 'sans-serif' )
+        .attr( 'font-size', '12px' )
+        .attr( 'text-anchor', 'middle' );
 }, false );
