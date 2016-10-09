91b7835fcc09b1516e97f11b68fbc9af010f9ab7
Valery Semenencko <gearmobile@gmail.com>
2016-10-09 21:47:28 +0300

    callback

--- d3js-subcase/subcase-04/app.js
+++ d3js-subcase/subcase-04/app.js
@@ -1,32 +1,28 @@
 window.addEventListener( 'load', function () {
-    // METHOD JSON
+    //
     let urlJSON = '../library/test.json';
     d3.json( urlJSON, function ( error, rawData ) {
         console.log( rawData[0] );
     });
-    console.log( 'Hello from D3.js!' ); // async!
-    // METHOD TSV
+    //
     let urlTSV = '../library/test.tsv';
     d3.tsv( urlTSV, function ( error, rawData ) {
         console.log( rawData[0] );
     });
-    // METHOD CSV
+    //
     let urlCSV = '../library/test.csv';
     d3.csv( urlCSV, function ( error, rawData ) {
         console.log( rawData[0] );
     });
-    // FILTER DATA
+    //
     d3.csv( urlCSV, function ( error, rawData ) {
-        let data = rawData.map( function( d ) {
-            "use strict";
+        let data = rawData.map( function ( d ) {
             return {
                 keyValue1: +d.Episode,
                 keyValue2: d.Title,
                 keyValue3: +d.USViewers
-            };
+            }
         });
         console.log( data );
     });
-    // GeoJSON
-    // TopoJSON
 }, false );
