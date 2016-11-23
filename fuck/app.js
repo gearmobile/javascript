0c205fa7ebc9b264c3b301c1aa9d304a8d974f0a
gearmobile <gearmobile@gmail.com>
2016-11-23 14:45:51 +0300

    add fuck

---
+++ fuck/app.js
@@ -0,0 +1,8 @@
+window.addEventListener( 'load', function () {
+    // --------------------------------------------------
+    const blockUno = document.querySelector( '#uno' );
+    const blockDuo = document.querySelector( '#duo' );
+    const blockTre = document.querySelector( '#tre' );
+    // --------------------------------------------------
+    console.log( blockUno.clientHeight + ' : ' + blockUno.clientWidth );
+}, false);
