386eb162aa65e1ae98623d902b7408594cfef620
gearmobile <gearmobile@gmail.com>
2016-12-01 12:40:27 +0300

    c3

---
+++ c3-tars/markup/components/chart/data/data.js
@@ -0,0 +1,8 @@
+var data = {
+    chart: {
+        title: 'C3.js - D3-based reusable chart library'
+    },
+    info: {
+        text: 'C3 enables deeper integration of charts into your application.'
+    }
+};
---
+++ c3-tars/markup/components/example/data/data.js
@@ -0,0 +1,11 @@
+example: {
+    heading: {
+        title: 'Hello from Handlebars'
+    },
+    author: {
+        age: '25',
+        gender: 'male',
+        name: 'John',
+        surname: 'Aaronson'
+    }
+}
