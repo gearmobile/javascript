eb20da5c74835fdb4fe868e95a4784d92cc6799d
gearmobile <gearmobile@gmail.com>
2016-12-12 21:37:30 +0300

    typescript

---
+++ typescript/case-01/app.js
@@ -0,0 +1,14 @@
+var ageValue = 25;
+var nameValue = 'Valery';
+var user = {
+    age: number = 25,
+    name: string = 'Valery'
+};
+var arr = ['uno', 'duo', 'tre'];
+// ------------------------------------------
+for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+    var val = arr_1[_i];
+    console.log(val);
+}
+console.log(user.name);
+//# sourceMappingURL=app.js.map
---
+++ typescript/case-01/app.ts
@@ -0,0 +1,16 @@
+const ageValue:number = 25;
+const nameValue:string = 'Valery';
+
+const user = {
+    age:number = 25,
+    name:string = 'Valery'
+};
+
+const arr = [ 'uno', 'duo', 'tre' ];
+
+// ------------------------------------------
+for ( let val of arr ) {
+    console.log( val );
+}
+
+console.log( user.name );
